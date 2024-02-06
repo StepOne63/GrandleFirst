0fc5f468041ba984e504da4e5830c82453ada28e
Step X <stepan170297@gmail.com>
2024-02-06 10:10:16 +0400

    first commit

---
+++ src/test/java/org/netology/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+package org.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(actual, expected);
+    }
+}
